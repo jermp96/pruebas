9a6cbb28e3fe489bac616ff01068c4a4c3f11648
Jeremy <jjeremyperez96@gmail.com>
2022-10-04 12:37:06 -0600

    *probando arreglos

---
+++ src/app/basicas/arreglos/arreglos.spec.ts
@@ -0,0 +1,14 @@
+import { obtenerRobots } from './arreglos';
+
+describe('pruebas de arreglos', () => {
+  it('debe de retornar al menos 3 robots', () => {
+    const res = obtenerRobots();
+    expect(res.length).toBeGreaterThanOrEqual(3);
+  });
+
+  it('debe de existir EVA-01 Y EVA-02', () => {
+    const res = obtenerRobots();
+    expect(res).toContain('EVA-01');
+    expect(res).toContain('EVA-02');
+  });
+});
---
+++ src/app/basicas/arreglos/arreglos.ts
@@ -0,0 +1,3 @@
+export function obtenerRobots(): string[] {
+  return ['EVA-01', 'FRANXXX', 'GUNDAN'];
+}
