97567c446d175048af173b925978e100e79a75bf
Jeremy <jjeremyperez96@gmail.com>
2022-10-04 16:23:30 -0600

    *validaciones de formulario

--- src/app/intermedio/formularios/formulario.spec.ts
+++ src/app/intermedio/formularios/formulario.spec.ts
@@ -8,5 +8,17 @@ describe('formulario', () => {
   it('Debe de crear un formulario con dos campos, email y password', () => {
     expect(componente.form.contains('email')).toBeTruthy();
     expect(componente.form.contains('password')).toBeTruthy();
-  })
+  });
+
+  it('email debe de ser obligatorio', () => {
+    const control = componente.form.get('email');
+    control?.setValue('');
+    expect(control?.valid).toBeFalsy();
+  });
+
+  it('email debe de ser un correo válido', () => {
+    const control = componente.form.get('email');
+    control?.setValue('jeremy@gmail.com');
+    expect(control?.valid).toBeTruthy();
+  });
 });
