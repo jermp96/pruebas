421f2a03eecd597e323339e2d1b3ce15c2c1f178
Jeremy <jjeremyperez96@gmail.com>
2022-10-04 16:58:16 -0600

    *espías

--- src/app/intermedio/espias/medicos.component.spec.ts
+++ src/app/intermedio/espias/medicos.component.spec.ts
@@ -1,3 +1,5 @@
+
+import { from } from 'rxjs';
 import { MedicosComponent } from './medicos.component';
 import { MedicosService } from './medicos.service';
 
@@ -5,15 +7,22 @@ import { MedicosService } from './medicos.service';
 describe('MedicosComponent', () => {
 
     let componente: MedicosComponent;
+    const servicio = new MedicosService(null as any);
 
     beforeEach( () => {
-        
+        componente = new MedicosComponent(servicio);
     });
 
 
-    it('', () => {
-
-   
+    it('Init: Debe de cargar los medicos', () => {
+        const medicos = ['medico 1', 'medico 2', 'medico 3'];
+        
+        spyOn(servicio, 'getMedicos').and.callFake(() => {
+            return from(medicos);
+        })
+        
+        componente.ngOnInit();
+        expect(componente.medicos.length).toBeGreaterThan(0);
     });
 
 
--- src/app/intermedio/espias/medicos.service.ts
+++ src/app/intermedio/espias/medicos.service.ts
@@ -1,26 +1,21 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-import 'rxjs/add/operator/map';
-
-@Injectable()
+@Injectable() 
 export class MedicosService {
 
   constructor( public http: HttpClient) { }
 
   getMedicos() {
-    return this.http.get('...')
-                .map( resp => resp['medicos'] );
+    return this.http.get('...');
   }
 
   agregarMedico( medico: any ) {
-    return this.http.post('...', medico )
-                .map( resp => resp['medico'] );
+    return this.http.post('...', medico );
   }
 
   borrarMedico( id: string ) {
-    return this.http.delete('...' )
-                .map( resp => resp['medico'] );
+    return this.http.delete('...' );
   }
 
 
