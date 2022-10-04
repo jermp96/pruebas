5ebe1b1c10ca8041c66c62397db68cd7bc2be9df
Jeremy <jjeremyperez96@gmail.com>
2022-10-04 15:26:55 -0600

    *Event Emitter

---
+++ src/app/intermedio/eventEmitter/jugador2.spec.ts
@@ -0,0 +1,31 @@
+import { Jugador2 } from './jugador2'
+
+describe('Jugador2 Emit', () => {
+  let jugador: Jugador2;
+
+  beforeEach(() => jugador = new Jugador2());
+
+  it('debe de emitir un evento cuando recibie daño', () => {
+    let nuevoHP = 0
+    jugador.hpCambia.subscribe(hp =>{
+      nuevoHP = hp;
+    });
+
+    jugador.recibeDanio(1000);
+    expect(nuevoHP).toBe(0);
+  });
+
+  it('debe de emitir un evento cuando recibie daño y sobrevivir si es menos de 100', () => {
+    let nuevoHP = 0
+    jugador.hpCambia.subscribe(hp =>{
+      nuevoHP = hp;
+    });
+    /**
+     * a pesar de que el subscribe es asincrono, el test espera 
+     * a que esta termine.
+     */
+
+    jugador.recibeDanio(50);
+    expect(nuevoHP).toBe(50);
+  });
+})
