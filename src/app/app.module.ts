e2524ddb0b0d8292f35f44b185b2f2067d0de65f
Jeremy <jjeremyperez96@gmail.com>
2022-10-10 12:14:56 -0600

    *material de pruebas de integracion-incrementador

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,15 +5,20 @@ import { AppComponent } from './app.component';
 import { MedicosComponent } from './intermedio/espias/medicos.component';
 import { MedicoComponent } from './intermedio2/medico/medico.component';
 import { HospitalComponent } from './intermedio2/hospital/hospital.component';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IncrementadorComponent } from './intermedio2/incrementador/incrementador.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MedicosComponent,
     MedicoComponent,
-    HospitalComponent
+    HospitalComponent,
+    IncrementadorComponent
   ],
   imports: [
+    FormsModule,
     BrowserModule
   ],
   providers: [],
